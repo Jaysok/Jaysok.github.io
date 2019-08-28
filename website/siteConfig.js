103547e181c1ad7105de75cc14cf123f771de6e8
Jaysok <wooljs@gmail.com>
2019-08-28 17:41:09 +0900

    siteconfig

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -30,7 +30,7 @@ const siteConfig = {
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'log4jay',
+  projectName: 'jaysok.github.io',
   organizationName: 'jaysok',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
